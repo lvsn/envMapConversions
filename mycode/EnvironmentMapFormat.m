b8897b6446763f8dfbb042bda605390eeb13fdfe
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2012-11-07 13:10:24 -0500

    Comments on EnvironmentMapFormat.

--- mycode/EnvironmentMapFormat.m
+++ mycode/EnvironmentMapFormat.m
@@ -1,14 +1,24 @@
 classdef EnvironmentMapFormat
-    %UNTITLED13 Summary of this class goes here
-    %   Detailed explanation goes here
+    % Represents the various kinds of environment map formats
+    %   
+    %   Currently:
+    %   - LatLong (latitude-longitude)
+    %   - Angular
+    %   - Cube
+    %   - SkyAngular 
+    %       (just the top half of the hemisphere, looking towards zenith)
     
     enumeration
         LatLong, Angular, Cube, SkyAngular
     end
     
     methods (Static)
-        % useful method to convert from string to class format
+        
         function f = str2format(str)
+            % Useful method to convert from string to class format
+            %   
+            %   f = str2format(str)
+            %
             switch lower(str)
                 case 'latlong'
                     f = EnvironmentMapFormat.LatLong;
