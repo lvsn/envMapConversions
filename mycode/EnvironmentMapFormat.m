63e574e9c1670b32e542eb8dc10ca7d152886fe1
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2012-11-07 13:08:56 -0500

    Added static function to generate a format from a string.

--- mycode/EnvironmentMapFormat.m
+++ mycode/EnvironmentMapFormat.m
@@ -5,5 +5,27 @@ classdef EnvironmentMapFormat
     enumeration
         LatLong, Angular, Cube, SkyAngular
     end
+    
+    methods (Static)
+        % useful method to convert from string to class format
+        function f = str2format(str)
+            switch lower(str)
+                case 'latlong'
+                    f = EnvironmentMapFormat.LatLong;
+                    
+                case 'angular'
+                    f = EnvironmentMapFormat.Angular;
+                    
+                case 'cube'
+                    f = EnvironmentMapFormat.Cube;
+                    
+                case 'skyangular'
+                    f = EnvironmentMapFormat.SkyAngular;
+                    
+                otherwise
+                    error('Unknown string: %s\n', str);
+            end
+        end
+    end
 end
 
