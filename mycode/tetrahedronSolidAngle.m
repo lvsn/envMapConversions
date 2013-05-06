f6e516be51ed508accd55cb2797e796f43482059
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-05-06 09:27:16 -0400

    Now supporting both methods from Wikipedia's article.
    L'Huillier's method is much faster to run because of parallelization (the other method requires computing the determinant, which is done in a stupid sequential way. Could also be sped up I guess).

--- mycode/tetrahedronSolidAngle.m
+++ mycode/tetrahedronSolidAngle.m
@@ -1,4 +1,4 @@
-function omega = tetrahedronSolidAngle(a, b, c)
+function omega = tetrahedronSolidAngle(a, b, c, varargin)
 % Computes the solid angle subtended by a tetrahedron.
 %
 %   omega = tetrahedronSolidAngle(a, b, c)
@@ -7,50 +7,52 @@ function omega = tetrahedronSolidAngle(a, b, c)
 % vertices of the triangle with respect to an origin.
 %
 % For more details, see:
-%   http://en.wikipedia.org/wiki/Solid_angle#Tetrahedron  
+%   http://en.wikipedia.org/wiki/Solid_angle#Tetrahedron
 %
-% We use L'Huilier's theorem because it's simpler to parallelize
-% 
+% Both methods are implemented, but L'Huillier (default) is easier to
+% parallelize and thus much faster. 
+%
 % ----------
 % Jean-Francois Lalonde
+
+lhuillier = true;
+
+parseVarargin(varargin{:});
 
 assert(size(a, 1) == 3, 'a must be a 3xN matrix');
 assert(size(b, 1) == 3, 'b must be a 3xN matrix');
 assert(size(c, 1) == 3, 'c must be a 3xN matrix');
 
-theta_a = acos(sum(b.*c, 1));
-theta_b = acos(sum(a.*c, 1));
-theta_c = acos(sum(a.*b, 1));
-
-theta_s = (theta_a + theta_b + theta_c)./2;
-
-product = tan(theta_s/2).*tan((theta_s-theta_a)./2).*...
-    tan((theta_s-theta_b)./2).*tan((theta_s-theta_c)./2);
-
-% account for negatives (not sure what to do here?)
-product(product<0) = 0;
-tmp = sqrt(product);
-
-omega = 4.*atan(tmp);
-
-return;
-
-
-a = column(a(:,51));
-b = column(b(:,51));
-c = column(c(:,51));
-
-al = norm(a); a = a./al;
-bl = norm(b); b = b./bl;
-cl = norm(c); c = c./cl;
-
-determ = det([a b c]);
-
-div = al*bl*cl + (a'*b)*cl + (a'*c)*bl + (b'*c)*al;
-at = atan2(determ, div);
-
-at(at<0) = at(at<0) + pi; % If det > 0 and div < 0 arctan2 returns < 0, so add pi.
-
-omega = 2*at;
+if lhuillier
     
- 
+    theta_a = acos(sum(b.*c, 1));
+    theta_b = acos(sum(a.*c, 1));
+    theta_c = acos(sum(a.*b, 1));
+    
+    theta_s = (theta_a + theta_b + theta_c)./2;
+    
+    product = tan(theta_s/2).*tan((theta_s-theta_a)./2).*...
+        tan((theta_s-theta_b)./2).*tan((theta_s-theta_c)./2);
+    
+    % account for negatives (not sure what to do here?)
+    product(product<0) = 0;
+    tmp = sqrt(product);
+    
+    omega = 4.*atan(tmp);
+    
+else
+    al = sqrt(sum(a.^2, 1)); 
+    bl = sqrt(sum(b.^2, 1)); 
+    cl = sqrt(sum(c.^2, 1)); 
+    
+    % determ = det([a b c]);
+    determ = arrayfun(@(i) det([a(:,i), b(:,i), c(:,i)]), 1:size(a,2));
+    
+    div = al.*bl.*cl + sum(a.*b,1).*cl + sum(a.*c,1).*bl + sum(b.*c,1).*al;
+    at = atan2(determ, div);
+    
+%     at(at<0) = at(at<0) + pi; % If det > 0 and div < 0 arctan2 returns < 0, so add pi.
+    
+    omega = 2*at;
+    
+end
